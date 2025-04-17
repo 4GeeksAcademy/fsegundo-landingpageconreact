a138041471135b94d5debe7615f21b019364128a
Segundo Ferreira <segundo.ferreira28@gmail.com>
2025-04-17 18:12:17 +0000

    3er cambio

--- src/js/Card.jsx
+++ src/js/Card.jsx
@@ -3,13 +3,15 @@ import React from "react";
 export const Card = () => {
     return (
 
-<div className="card" style={{width: '18rem'}}>
-					<img src="..." className="card-img-top" alt="..." />
-					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
-						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" className="btn btn-primary">Go somewhere</a>
-					</div>
-				</div>
+        
+            <div className="cardtattoo" style={{ width: '18rem', display: 'flex-wrap', justifyContent: 'center'}}  >
+                <img src="https://preview.redd.it/bold-and-vibrant-tattoo-work-by-argentinian-artist-alejo-v0-3k0i09whvvle1.jpg?width=1080&crop=smart&auto=webp&s=feab0b1480c12cecd371eca8ac132936a773d45f" className="card-img-top" alt="imagen no disponible" />
+                <div className="card-body">
+                    <h5 className="card-title"></h5>
+                    <p className="card-text"></p>
+                    <a href="#" className="btn btn-primary"></a>
+                </div>
+            </div>
+        
     )
 }
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -5,15 +5,53 @@ import { Jumbotron } from "../Jumbotron";
 
 //include images into your bundle
 
+const stockcards = [{
+	title: 'Panter',
+	description: 'The best in the jungle',
+	button: 'Press here'
+
+},
+{
+	title: 'Lion',
+	description: 'The king of the jungle',
+	button: 'Press here'
+
+},
+{
+	title: 'Marcian',
+	description: 'The last one in the earth',
+	button: 'Press here'
+
+},
+{
+	title: 'Monkey',
+	description: 'Spicy boy',
+	button: 'Press here'
+
+}
+
+]
 
 //create your first component
 const Home = () => {
 	return (
 		<>
-		<Navbar/>
-		<Jumbotron/>
-			<Card />
-			
+
+			<Navbar />
+			<Jumbotron />
+			{
+				stockcards.map((element, id) => {
+					return (
+						<Card key={id} />
+					)
+				})
+			}
+			<footer className="bg-light text-center text-lg-start mt-auto">
+				<div className="text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}>
+					© 2025 by <strong>Alejo Gmz</strong>
+				</div>
+			</footer>
+
 		</>
 
 	);
